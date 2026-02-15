88e55c880ebc1a345667d6021449574ffa9765bf
NottVK <pelaezkevin07@gmail.com>
2026-02-15 11:19:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -118,6 +118,7 @@ if(canvas){
  
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  import{getMessaging,getToken,onMessage} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-messaging.js"
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
