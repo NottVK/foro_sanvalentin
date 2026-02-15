6ccb5aab278f78b15812952d1f065387251e2b77
NottVK <pelaezkevin07@gmail.com>
2026-02-15 12:07:24 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,53 +1,45 @@
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-  import {getMessaging,getToken,onMessage} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-messaging.js"
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-messaging.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAmYBxuRWpIIIOLyAae9eJLzVa6RVbNtHw",
-    authDomain: "vaalkev-94f3e.firebaseapp.com",
-    projectId: "vaalkev-94f3e",
-    storageBucket: "vaalkev-94f3e.firebasestorage.app",
-    messagingSenderId: "421450061531",
-    appId: "1:421450061531:web:d832bc1977905bae0b1efe"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAmYBxuRWpIIIOLyAae9eJLzVa6RVbNtHw",
+  authDomain: "vaalkev-94f3e.firebaseapp.com",
+  projectId: "vaalkev-94f3e",
+  storageBucket: "vaalkev-94f3e.firebasestorage.app",
+  messagingSenderId: "421450061531",
+  appId: "1:421450061531:web:d832bc1977905bae0b1efe"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const messaging =getMessaging(app);
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
 
- 
+async function iniciarFirebase() {
+  try {
+    const registration = await navigator.serviceWorker.register("./firebase-messaging-sw.js");
 
-navigator.serviceWorker.register('./firebase-messaging-sw.js')
-.then((registration) => {
-  getToken(messaging, {
-    vapidKey: "BIoVsDy4PZBw0AP6iOUGIQvX4i29X3muX-v3eEoDM1NS_HgHv8CgswymHoThYiDRPiGvi4HKixKUT5UguhW6fmI",
-    serviceWorkerRegistration: registration
-  }).then((currentToken) => {
-    console.log("TOKEN:", currentToken);
-  });
-});
+    const permission = await Notification.requestPermission();
 
-
-  // Pedir permiso
-  Notification.requestPermission().then((permission) => {
     if (permission === "granted") {
-      getToken(messaging, {
-        vapidKey: "BIoVsDy4PZBw0AP6iOUGIQvX4i29X3muX-v3eEoDM1NS_HgHv8CgswymHoThYiDRPiGvi4HKixKUT5UguhW6fmI"
-      }).then((currentToken) => {
-        if (currentToken) {
-          console.log("TOKEN DEL TELEFONO:");
-          console.log(currentToken);
-        } else {
-          console.log("No se generó token");
-        }
+      const currentToken = await getToken(messaging, {
+        vapidKey: "BIoVsDy4PZBw0AP6iOUGIQvX4i29X3muX-v3eEoDM1NS_HgHv8CgswymHoThYiDRPiGvi4HKixKUT5UguhW6fmI",
+        serviceWorkerRegistration: registration
       });
+
+      if (currentToken) {
+        console.log("TOKEN DEL TELEFONO:");
+        console.log(currentToken);
+      } else {
+        console.log("No se generó token");
+      }
     }
-  });
 
-  onMessage(messaging, (payload) => {
-    console.log("Mensaje recibido en primer plano:", payload);
-  });
+  } catch (error) {
+    console.error("Error registrando service worker:", error);
+  }
+}
 
+iniciarFirebase();
+
+onMessage(messaging, (payload) => {
+  console.log("Mensaje recibido en primer plano:", payload);
+});
